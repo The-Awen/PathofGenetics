ca0099f2cbb76654ba60c51a17cca03afb150b5a
sremedios <sam.remedios@gmail.com>
2018-05-05 17:44:02 -0500

    can now determine starts, and generate random trees. todo exists on line 218

--- GeneticMicroservice/src/main.rs
+++ GeneticMicroservice/src/main.rs
@@ -1,14 +1,16 @@
+#![feature(vec_remove_item)]
+
 extern crate serde;
 extern crate serde_json;
-
 #[macro_use]
 extern crate serde_derive;
+
+extern crate rand;
 
 use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashMap;
-use serde_json::{Value, Error};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Parent {
@@ -71,6 +73,10 @@ struct Node {
     da: u32,
     ia: u32,
     out: Vec<u32>,
+    #[serde(default)]
+    isAscendancyStart: bool,
+    #[serde(default)]
+    adjacencies: Vec<u32>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -93,17 +99,15 @@ fn main() {
     
     let deserialized: Parent = serde_json::from_str(&contents).unwrap();
 
-    /*
-
-    let mut adjacencies: HashMap<u32, Vec<u32>> = HashMap::new();
     
-    // ********** BUILD ADJACENCIES ********** \\ 
-    for node in deserialized.nodes { 
-        adjacencies.insert(node.id, node.out);
-    }
+    // ********** CALCULATE ADJACENCIES ********** \\ 
 
-    //println!("{:#?}", adjacencies);
-    */
+    let adjacencies = calculate_adjacencies(&deserialized);
+
+    let first = 54307;
+    let second = 12401;
+    //println!("{}: {:?}, {}: {:?}", first, adjacencies[&first], second, adjacencies[&second]);
+
 
     // ********** BUILD GRAPH ********** \\ 
     //write_adjacency_file(adjacencies);
@@ -115,26 +119,65 @@ fn main() {
     let mut start_ids: HashMap<String, Vec<u32>> = HashMap::new();
 
     let class_names = vec!["WITCH", "SIX", "RANGER", "DUELIST", "MARAUDER", "TEMPLAR"];
-    for node in deserialized.nodes { 
+    let mut ascendancy_ids: Vec<u32> = Vec::new();
+    for mut node in deserialized.nodes { 
+
+        if node.isAscendancyStart {
+            ascendancy_ids.push(node.id);
+        }
+
+        // ********** ADD ADJACENCIES TO NODES ********** \\ 
+        node.adjacencies = adjacencies[&node.id].clone();
+
         if class_names.contains(&&node.dn[..]) {
             match &*node.dn {
-                "SIX" => start_ids.insert("SHADOW".to_string(), node.out),
-                _ => start_ids.insert(node.dn, node.out),
+                "SIX" => start_ids.insert("SHADOW".to_string(), node.adjacencies),
+                _ => start_ids.insert(node.dn, node.adjacencies),
             };
         }
     }
 
-    // EDGE CASES
     
     // group 93 has a vector called n. This is the scion start, including pathway to ascendant.
-    start_ids.insert("SCION".to_string(), deserialized.groups[&93].n.clone());
+    let mut scion_next: Vec<u32> = deserialized.groups[&93].n.clone();
+    scion_next.remove_item(&58833);
+    start_ids.insert("SCION".to_string(), scion_next);
+    // also remove dummy head for scion
+    //start_ids[&"SCION".to_string()].remove(58833);
 
-    // the shadow start is missing a node, adding manually
-    start_ids.get_mut("SHADOW").unwrap().push(17788 as u32);
-    
+    // ********** PRUNE OUT ASCENDANCY CONNECTIONS ********** \\ 
+    for (_key, val) in start_ids.iter_mut() {
+       val.retain(|&x| !ascendancy_ids.contains(&x));
+    }
 
-    println!("{:#?}", start_ids);
+    //println!("{:#?}", start_ids);
 
+
+    let tree: Vec<u32> = gen_random_tree(adjacencies, &start_ids[&"DUELIST".to_string()]);
+
+    println!("{:?}", tree);
+}
+
+// Build a hashmap mapping each node to its adjacent nodes. This double-connects
+// each node such that the value of the map 
+fn calculate_adjacencies(parent: &Parent) -> HashMap<u32, Vec<u32>> {
+    let mut adjacencies: HashMap<u32, Vec<u32>> = HashMap::new();
+
+    for node in &parent.nodes { 
+        adjacencies.insert(node.id, node.out.clone());
+    }
+
+    let adj_copy = adjacencies.clone();
+
+    for (key, value) in adj_copy {
+        for n in value {
+            if !adjacencies[&n].contains(&key) {
+                adjacencies.get_mut(&n).unwrap().push(key);
+            }
+        }
+    }
+
+    adjacencies
 }
 
 fn write_adjacency_file(adjacencies: HashMap<String, Vec<u32>>) {
@@ -167,4 +210,46 @@ fn write_adjacency_file(adjacencies: HashMap<String, Vec<u32>>) {
     }
     buffer.write(b"}\n").unwrap();
 
+}
+
+// Generate a random tree. The possible_starts refers to a specified class
+fn gen_random_tree(adjacencies: HashMap<u32, Vec<u32>>, possible_starts: &Vec<u32>) -> Vec<u32> {
+
+    // TODO: be able to branch out from previous trees as well, instead of the most recent one
+    // this should be a pretty easy fix when handling "possible_nexts"
+
+    let mut cur_tree: Vec<u32> = Vec::new();
+    let mut count = 0;
+    let THRESHOLD = 123;
+
+    // roll a random number to choose possible start
+    let rand_roll = rand::random::<usize>() % possible_starts.len();
+    
+    // allocate that node
+    cur_tree.push(possible_starts[rand_roll]);
+    count += 1;
+
+    while count < THRESHOLD {
+
+        let last_idx: usize = cur_tree.len() - 1;
+        let adj_idx: u32 = cur_tree[last_idx];
+        // find that node's adjacencies: ie: next possible nodes
+        let mut possible_nexts: Vec<u32> = adjacencies[&adj_idx].clone();
+        
+        // don't choose previously allocated nodes as possibilities
+        possible_nexts.retain(|&x| !cur_tree.contains(&x));
+
+        // exit if no further choices from this node
+        if possible_nexts.len() == 0 {
+            return cur_tree;
+        }
+        
+        // roll again
+        let rand_roll = rand::random::<usize>() % possible_nexts.len();
+
+        // allocate new random choice
+        cur_tree.push(possible_nexts[rand_roll]);
+    }
+
+    cur_tree
 }
